2bcc0e3130e2807bfdfaaa2d3e660479979f051b
Sanjida Akter <137642044+Sanjida51@users.noreply.github.com>
2024-08-10 23:04:06 +0600

    Add files via upload

---
+++ Assignment3/index.js
@@ -0,0 +1,32 @@
+//let str = "Hello, World!";
+//console.log(str.length);
+//let str = "Hello, World!";
+//console.log(str[7]); // Logs 'W'
+/*let str1 = "Hello, ";
+let str2 = "World!";
+let result = str1 + str2;
+console.log(result);*/
+/*let str = "Hello, World!";
+console.log(str.toUpperCase());*/
+
+/*let str = "Hello, world!";
+let extracted = str.slice(7, 12);
+console.log(extracted);*/
+
+/*let name = "John";
+let age = 30;
+let message = `My name is ${name} and I am ${age} years old.`;
+console.log(message);*/
+
+/*let str = "123.45";
+let num = parseFloat(str);
+console.log(num);
+*/
+//let num = 4.7;
+//console.log(Math.round(num));
+
+
+console.log(Number.NaN);
+console.log(Number.MIN_VALUE);
+console.log(Number.MAX_VALUE);
+
